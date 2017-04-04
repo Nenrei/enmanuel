234c23b919ab105a4af1853361df99fee4e11292
Nenrei <enmanuelvalencia@outlook.com>
2017-04-04 09:26:17 +0200

    Internationalization
    Added the galician language.

--- src/js/app.js
+++ src/js/app.js
@@ -37,7 +37,8 @@ angular.module("myApp").config(function($routeProvider, $locationProvider, $tran
         OPTION: {
             TRANSLATE: {
                 EN: "English",
-                ES: "Spanish"
+                ES: "Spanish",
+                GA: "Galician"
             }
         },
         MADEWITH: {
@@ -57,7 +58,7 @@ angular.module("myApp").config(function($routeProvider, $locationProvider, $tran
                 SUB_DESC: "'Learn Unity 5: from zero to expert' Udemy's course."
             },
             {
-                DESC: "Website to storage of paper toys of manga, anime and videogames.",
+                DESC: "Website for storing manga and anime character paper toys.",
                 SUB_DESC: ""
             }
         ],
@@ -68,7 +69,7 @@ angular.module("myApp").config(function($routeProvider, $locationProvider, $tran
             TEC_KNOWLEDGE: "Tec. Knowledge",
             OTHER: "Other Data of Interest",
             PERS_DATA: {
-                BIRTH: "08 September 1992, La Coruña, Galicia, Spain"
+                BIRTH: "08 September 1992, La Coru&ntilde;a, Galicia, Spain"
             },
             JOBS: [
                 {
@@ -81,7 +82,7 @@ angular.module("myApp").config(function($routeProvider, $locationProvider, $tran
                 {
                     DURATION: "3 years",
                     CENTER: "Indra Software Labs",
-                    PLACE: "La Coruña",
+                    PLACE: "La Coru&ntilde;a",
                     JOB: "Frontend developer",
                     DESC: "Development of InCMS, a web application to manage all the commercial cicle of an energy company(payment, billing, customer service, etc)."
                 }
@@ -89,7 +90,7 @@ angular.module("myApp").config(function($routeProvider, $locationProvider, $tran
             EDUC: [
                 {
                     DURATION: "2 years",
-                    CENTER: "CIFP Rodolfo Ucha Piñeiro",
+                    CENTER: "CIFP Rodolfo Ucha Pi&ntilde;eiro",
                     PLACE: "Ferrol",
                     JOB: "Senior Technician in Multiplatform Application Development"
                 }
@@ -128,7 +129,8 @@ angular.module("myApp").config(function($routeProvider, $locationProvider, $tran
         OPTION: {
             TRANSLATE: {
                 EN: "Ingl&eacute;s",
-                ES: "Espa&ntilde;ol"
+                ES: "Espa&ntilde;ol",
+                GA: "Gallego"
             }
         },
         MADEWITH: {
@@ -140,55 +142,55 @@ angular.module("myApp").config(function($routeProvider, $locationProvider, $tran
                 SUB_DESC: "Curso 'Aprende Unity 5: de cero a experto' de Udemy."
             },
             {
-                DESC: "Clásico juego de naves espaciales en el que tendremos que destruir las naves enemigas y permanecer vivos el máximo tiempo posible.",
+                DESC: "Cl&aacute;sico juego de naves espaciales en el que tendremos que destruir las naves enemigas y permanecer vivos el m&aacute;ximo tiempo posible.",
                 SUB_DESC: "Curso 'Aprende Unity 5: de cero a experto' de Udemy."
             },
             {
-                DESC: "En este juego un curioso npc nos asignará una misión que debemos completar.",
+                DESC: "En este juego un curioso npc nos asignar&aacute; una misi&oacute;n que debemos completar.",
                 SUB_DESC: "Curso 'Aprende Unity 5: de cero a experto' de Udemy."
             },
             {
-                DESC: "Sitio web para el almacenamiento de figuras de papel de personajes manga, anime y videojuegos.",
+                DESC: "Sitio web para almacenar figuras de papel de personajes manga y anime.",
                 SUB_DESC: ""
             }
         ],
         CURRIC:{
-            EDUCATION: "Educación",
+            EDUCATION: "Educaci&oacute;n",
             EXPERIENCE: "Experiencia",
-            COMP_EDUCATION: "Educación Comp.",
-            TEC_KNOWLEDGE: "Conocimientos Técnicos",
-            OTHER: "Otros Datos de Interés",
+            COMP_EDUCATION: "Educaci&oacute;n Comp.",
+            TEC_KNOWLEDGE: "Conocimientos T&eacute;cnicos",
+            OTHER: "Otros Datos de Inter&eacute;s",
             PERS_DATA: {
-                BIRTH: "09 Septiembre 1992, La Coruña, Galicia, España"
+                BIRTH: "09 Septiembre 1992, La Coru&ntilde;a, Galicia, Espa&ntilde;a"
             },
             JOBS: [
                 {
                     DURATION: "2 meses",
-                    CENTER: "Grupo Tecnológico Artabria",
+                    CENTER: "Grupo Tecnol&oacute;gico Artabria",
                     PLACE: "Ferrol",
-                    JOB: "Estudiante en Prácticas",
-                    DESC: "Desarrollo de dos sencillas aplicaciones para Android. Una para que los empleados pudieran ver las tareas asignadas y otra para poder consultar la facturación de una o varias empresas en gráficas."
+                    JOB: "Estudiante en Pr&aacute;cticas",
+                    DESC: "Desarrollo de dos sencillas aplicaciones para Android. Una para que los empleados pudieran ver las tareas asignadas y otra para poder consultar la facturaci&oacute;n de una o varias empresas en gr&aacute;ficas."
                 },
                 {
-                    DURATION: "3 años",
+                    DURATION: "3 a&ntilde;os",
                     CENTER: "Indra Software Labs",
-                    PLACE: "La Coruña",
+                    PLACE: "La Coru&ntilde;a",
                     JOB: "Desarrollador Frontend",
-                    DESC: "Desarrollo de InCMS, una aplicación web para gestionar todo el ciclo comercial de una empresa energética (pago, facturación, atención al cliente, etc.)."
+                    DESC: "Desarrollo de InCMS, una aplicaci&oacute;n web para gestionar todo el ciclo comercial de una empresa energ&eacute;tica (pago, facturaci&oacute;n, atenci&oacute;n al cliente, etc.)."
                 }
             ],
             EDUC: [
                 {
-                    DURATION: "2 años",
-                    CENTER: "CIFP Rodolfo Ucha Piñeiro",
+                    DURATION: "2 a&ntilde;os",
+                    CENTER: "CIFP Rodolfo Ucha Pi&ntilde;eiro",
                     PLACE: "Ferrol",
-                    JOB: "Técnico Superior en Desarrollo de Aplicaciones Multiplatforma"
+                    JOB: "T&eacute;cnico Superior en Desarrollo de Aplicaciones Multiplatforma"
                 }
             ],
             COMP_EDUC: {
                 JAVAEE6: "Desarrollo de Aplicaciones con Java EE6",
                 PATTERNS: "Patrones de Java EE5",
-                ANGULAR: "Introducción a AngularJS",
+                ANGULAR: "Introducci&oacute;n a AngularJS",
             },
             KNOWLEDGE: {
                 FRONT: "Desarrollo Frontend",
@@ -196,7 +198,7 @@ angular.module("myApp").config(function($routeProvider, $locationProvider, $tran
                 OTHER: "Otras Utilidades"
             },
             OTHER_DATA: {
-                DRIVE: "Permiso de conducir B y vehículo propio"
+                DRIVE: "Permiso de conducir B y veh&iacute;culo propio"
             }
         },
         FOOTER: {
@@ -205,10 +207,106 @@ angular.module("myApp").config(function($routeProvider, $locationProvider, $tran
         }
     };
 
+    var translationsGA = {
+        MENU: {
+            HOME: "Inicio",
+            CURRIC: "Curriculum",
+            WEB: "Webs",
+            GAMES: "Xogos",
+            LANG: "Idioma"
+        },
+        MAIN: {
+            WORK: "Desenvolvedor Frontend"
+        },
+        OPTION: {
+            TRANSLATE: {
+                EN: "Ingl&eacute;s",
+                ES: "Espa&ntilde;ol",
+                GA: "Galego"
+            }
+        },
+        MADEWITH: {
+            UNITY: "Desenvolto en Unity"
+        },
+        PROJECTS: [
+            {
+                DESC: "O obxectivo deste xogo é recoller todas as moedas do escenario.",
+                SUB_DESC: "Curso 'Aprende Unity 5: de cero a experto' de Udemy."
+            },
+            {
+                DESC: "Xogo de naves clásico onde tes que destruír naves inimigas e permanecer vivo o maior tempo posible.",
+                SUB_DESC: "Curso 'Aprende Unity 5: de cero a experto' de Udemy."
+            },
+            {
+                DESC: "Neste partido un curioso npc nos atribúe unha misión que debe ser completada.",
+                SUB_DESC: "Curso 'Aprende Unity 5: de cero a experto' de Udemy."
+            },
+            {
+                DESC: "Sitio web para almacear figuras de papel de personaxes manga e anime.",
+                SUB_DESC: ""
+            }
+        ],
+        CURRIC:{
+            EDUCATION: "Educaci&oacute;n",
+            EXPERIENCE: "Experiencia",
+            COMP_EDUCATION: "Educaci&oacute;n Comp.",
+            TEC_KNOWLEDGE: "Coñecementos T&eacute;cnicos",
+            OTHER: "Outros Datos de Intererese",
+            PERS_DATA: {
+                BIRTH: "09 Setembro 1992, A Coru&ntilde;a, Galicia, Espa&ntilde;a"
+            },
+            JOBS: [
+                {
+                    DURATION: "2 meses",
+                    CENTER: "Grupo Tecnol&oacute;xico Artabria",
+                    PLACE: "Ferrol",
+                    JOB: "Estudante en Pr&aacute;cticas",
+                    DESC: "Desenvolvemento de duos sencillas aplicacións para Android. Unha para que o persoal poida ver as tarefas asignadas e outra para poder consultar a facturaci&oacute;n de unha ou varias empresas en gr&aacute;ficas."
+                },
+                {
+                    DURATION: "3 anos",
+                    CENTER: "Indra Software Labs",
+                    PLACE: "A Coru&ntilde;a",
+                    JOB: "Desenvolvedor Frontend",
+                    DESC: "Desenvolvemento de InCMS, unha aplicaci&oacute;n web para xestionar todo o ciclo comercial de unha empresa enerx&eacute;tica (pago, facturaci&oacute;n, atenci&oacute;n ó cliente, etc.)."
+                }
+            ],
+            EDUC: [
+                {
+                    DURATION: "2 anos",
+                    CENTER: "CIFP Rodolfo Ucha Pi&ntilde;eiro",
+                    PLACE: "Ferrol",
+                    JOB: "T&eacute;cnico Superior en Desenvolvemento de Aplicacións Multiplatforma"
+                }
+            ],
+            COMP_EDUC: {
+                JAVAEE6: "Desenvolvemento de Aplicacións con Java EE6",
+                PATTERNS: "Patróns de Java EE5",
+                ANGULAR: "Introducci&oacute;n a AngularJS",
+            },
+            KNOWLEDGE: {
+                FRONT: "Desenvolvemento Frontend",
+                BACK: "Desenvolvemento Backend",
+                OTHER: "Outras Utilidades"
+            },
+            OTHER_DATA: {
+                DRIVE: "Permiso de conducir B e veh&iacute;culo propio"
+            }
+        },
+        FOOTER: {
+            IN_DEVELOP: "Web en Desenvolvemento.",
+            THEME: "Tema base 'Cover' por "
+        }
+    };
+
+
+
     $translateProvider.translations('en', translationsEN);
     $translateProvider.translations('es', translationsES);
-    $translateProvider.preferredLanguage('en');
-    $translateProvider.fallbackLanguage('en');
+    $translateProvider.translations('ga', translationsGA);
+
+    $translateProvider.preferredLanguage('es');
+    $translateProvider.fallbackLanguage('es');
 
 });
 
